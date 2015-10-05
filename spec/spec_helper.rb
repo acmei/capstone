9e21fef86d78768de948239a97efb699a6641f2a
Anita <asovay@gmail.com>
2015-10-05 15:04:21 -0700

    added additonal requirements for gems

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'factory_girl'
+require 'simplecov'
+
+SimpleCov.start 'rails'
+
+RSpec.configure do |config|
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
