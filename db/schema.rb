c6f65f062ae97b504391e49deb488aa9c0d49dbe
Anita <asovay@gmail.com>
2015-10-13 12:34:49 -0700

    added home controler and home index view, migrated user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151013192709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "username"
+    t.string   "password"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
