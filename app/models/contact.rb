7cd95e6eae7a870b74f964584be4ce32d3d18d25
Anita <asovay@gmail.com>
2015-10-15 15:57:48 -0700

    reworked contacts model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,3 @@
 class Contact < ActiveRecord::Base
-  belongs_to :therapist
+  belongs_to :user
 end
--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
   factory :contact do
-    contact1 "MyString"
-contact2 "MyString"
-therapist nil
+    contact1_name "MyString"
+contact1_phone "MyString"
+contact2_name "MyString"
+contact2_phone "MyString"
+user nil
   end
 
 end
