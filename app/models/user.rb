906de974df64c8f1f10feda6ed708389ab62f065
Anita <asovay@gmail.com>
2015-10-16 12:44:14 -0700

    added note about querying for users answers

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,7 @@ class User < ActiveRecord::Base
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
   validates :password,  presence: true, length: { minimum: 6 }
+
+  # QUERYING answers for user
+  # User.first.diaries.first.questions &:answers
 end
