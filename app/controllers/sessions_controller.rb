422bd2099e17c37173c3cc77b589445b45629d03
Anita <asovay@gmail.com>
2015-10-18 11:49:50 -0700

    added sessions controller, view, and routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:error] = "Incorrect email/password combination."
+      render 'new'
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # Custom Routes
+  get    "login"  => "sessions#new"
+  post   "login"  => "sessions#create"
+  delete "logout" => "sessions#destroy"
 end
