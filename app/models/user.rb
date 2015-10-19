f8fec3aeb3437508b21a925313e00a4fee0d7d6c
Anita <asovay@gmail.com>
2015-10-19 14:53:44 -0700

    added additional seed information for users and added activation token method for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 require 'securerandom'
 
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   # ASSOCIATIONS ---------------------------------------------------------------
   has_secure_password
@@ -57,16 +58,35 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
-  # OmniAuth
-  def self.find_or_create_from_omniauth(auth_hash)
-    uid = auth_hash[:uid]
-    provider = auth_hash[:provider]
+  private
 
-    user = User.where(uid: uid, provider: provider).first_or_initialize
-    user.email = auth_hash[:info][:email]
-    user.username = auth_hash[:info][:name]
-    user.password = SecureRandom.uuid
+    # OmniAuth
+    def self.find_or_create_from_omniauth(auth_hash)
+      uid = auth_hash[:uid]
+      provider = auth_hash[:provider]
 
-    return user.save ? user : nil 
-  end
+      user = User.where(uid: uid, provider: provider).first_or_initialize
+      user.email = auth_hash[:info][:email]
+      user.username = auth_hash[:info][:name]
+      user.password = SecureRandom.uuid
+
+      return user.save ? user : nil 
+    end
+
+    # Converts email to all lower-case
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
+    # Remembers a user in the database for use in persistent sessions.
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
 end
