991d91b99be4c56a786716a3fab573b3cd789601
Anita <asovay@gmail.com>
2015-10-19 11:08:05 -0700

    fixing logout and editing csv

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(username: params[:session][:username])
 
     if user && user.authenticate(params[:session][:password])
       log_in user
