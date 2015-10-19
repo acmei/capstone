5c6680f8a48e71eed16c3902ba5781b3203647f2
Anita <asovay@gmail.com>
2015-10-19 10:43:58 -0700

    adding therapist controller, view, signupform, routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users
+  resources :therapists
   resources :sessions, only: [:new, :create, :destroy]
 
   # Custom Routes
-  get    "signup" => "users#new"
-  get    "login"  => "sessions#new"
-  post   "login"  => "sessions#create"
-  delete "logout" => "sessions#destroy"
+  get    "signup"             => "users#new"
+  get    "therapist_signup"   => "therapists#new"
+  get    "login"              => "sessions#new"
+  post   "login"              => "sessions#create"
+  delete "logout"             => "sessions#destroy"
 end
