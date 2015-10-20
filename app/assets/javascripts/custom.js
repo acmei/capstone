7397658c6710c0771cefb9cce8ace8a53e6b988d
Anita <asovay@gmail.com>
2015-10-20 16:39:32 -0700

    preloader only loads once

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,14 @@
-$(document).ready(function() {
- 
-  setTimeout(function(){
+$(document).ready(function($) {
+  if ($.cookie('preloader')) {
+    $('#loader-wrapper').hide();
+  }
+  else {
+    setTimeout(function(){
       $('body').addClass('loaded');
       $('h1').css('color','#222222');
-  }, 1000);
- 
+    }, 1000);
+
+    // and now we create 7 day cookie
+    $.cookie('preloader', true, {path: '/', expire: 7});
+  }
 });
