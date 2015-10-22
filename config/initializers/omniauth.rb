a8fb2c91ed580e3b4cbbc3143d8b297811ce0b7b
Anita <asovay@gmail.com>
2015-10-21 18:19:05 -0700

    added ERD, removed google omniauth consent

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-    :prompt => "consent"
-  }
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
