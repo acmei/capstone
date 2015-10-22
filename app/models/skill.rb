9aecdf0b2dd6f227b5645525a5adb55013761231
Anita <asovay@gmail.com>
2015-10-22 09:31:23 -0700

    ordering of skill model validations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,10 +3,10 @@ class Skill < ActiveRecord::Base
   has_and_belongs_to_many :users, join_table: :skills_users
 
   # VALIDATIONS ----------------------------------------------------------------
-  validates :category,    presence: true,
-                          inclusion: { in: ["mindfulness", "IE", "ER", "DT"] }
   validates :text,        presence: true,
                           uniqueness: { case_sensitive: false }
+  validates :category,    presence: true,
+                          inclusion: { in: ["mindfulness", "IE", "ER", "DT"] }
   validates :acronym,     uniqueness: { case_sensitive: false }
   validates :description, presence: true,
                           uniqueness: { case_sensitive: false }
