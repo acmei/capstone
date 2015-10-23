e89d3cd327680024d5ca8a73dcf2f4f15c60372a
Anita <asovay@gmail.com>
2015-10-22 20:16:55 -0700

    edited question categories and added category scope

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,7 +9,8 @@ class Question < ActiveRecord::Base
   validates :category,    presence: true,
                           inclusion: { 
                             in: ["urge", "rating", "drugs/meds", "actions", 
-                                "emotions", "optional", "session", "skill"] }
+                                "emotions", "optional", "session_urge", 
+                                "session_reg", "session", "skill"] }
   validates :answer_type, presence: true,
                           inclusion: { in: ["num", "text", "num/text", "bool"] }
   validates :recurrence,  presence: true,
@@ -17,6 +18,6 @@ class Question < ActiveRecord::Base
                           inclusion: { in: [1, 7] }
 
   # SCOPES ---------------------------------------------------------------------
-  scope :published, -> { where(published: true) }
+  scope :category, -> (category) { where(category: category) }
 
 end
