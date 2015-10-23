c7198186b2774c5a1991aa34a9335774cd152551
Anita <asovay@gmail.com>
2015-10-22 17:46:17 -0700

    removed set default photo method, removed phone numbers from seed, removed .username method in emails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,11 @@ class UsersController < ApplicationController
     else
       return render :new_therapist
     end
-    # request.fullpath == "/signup" ? render :new : render :new_therapist
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      set_default_photo(@user)
       @user.send_activation_email
       flash[:notice] = "Please check your email to activate your account."
       redirect_to root_url
