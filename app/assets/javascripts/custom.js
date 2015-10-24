2144ead0c2bdb8a625835a45b12ab1a5275bd3a4
Anita <asovay@gmail.com>
2015-10-24 11:31:33 -0700

    added image hover effect on settings

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function($) {
+  // PRELOADER
   if ($.cookie('preloader')) {
     $('#loader-wrapper').hide();
   }
@@ -8,7 +9,19 @@ $(document).ready(function($) {
       $('h1').css('color','#222222');
     }, 1000);
 
-    // and now we create 7 day cookie
+    // create 1 day cookie
     $.cookie('preloader', true, {path: '/', expire: 1});
   }
+
+  // PHOTO SETTINGS
+  var currentSrc = $('.current-img').attr('src')
+
+  $('.img-options')
+    .mouseover(function() {
+      var previewSrc = $(this).attr('src');
+      $('.current-img').attr('src', previewSrc)
+    })
+    .mouseout(function() {
+      $('.current-img').attr('src', currentSrc)
+    });
 });
