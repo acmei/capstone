38eaac902e2c23774bdb2ec5c0d691e8ab8d5e48
Anita <asovay@gmail.com>
2015-10-27 09:52:39 -0700

    card flip in progress

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -24,4 +24,21 @@ $(document).ready(function($) {
     .mouseout(function() {
       $('.current-img').attr('src', currentSrc)
     });
+
+
+  // SKILLS CARD FLIP
+  (function() {
+    var cards = document.querySelectorAll(".card.effect-click");
+    for ( var i  = 0, len = cards.length; i < len; i++ ) {
+      var card = cards[i];
+      clickListener( card );
+    }
+
+    function clickListener(card) {
+      card.addEventListener( "click", function() {
+        var c = this.classList;
+        c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      });
+    }
+  })();
 });
